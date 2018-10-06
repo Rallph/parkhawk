be7d65fe5003f1252d7013d123c184033655a12d
Rallph <rallph@protonmail.com>
2018-10-06 14:15:01 -0500

    fix required attributes for DecimalField

--- parkhawk/parkmap/models.py
+++ parkhawk/parkmap/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class ParkingSpot(models.Model):
     location = models.CharField(max_length=140) # street, possibly address
-    price = models.DecimalField() # price of space
+    price = models.DecimalField(max_digits=5,decimal_places=2) # price of space
     hours_start_time = models.TimeField() # no way to represent time range that I could find
     hours_end_time = models.TimeField()   # so making start hour and end hour separate
     note = models.CharField(max_length=500) # note for listing creator to leave for others
