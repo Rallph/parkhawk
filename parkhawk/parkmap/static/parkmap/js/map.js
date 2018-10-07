0543e57da1829605411517476bb4cc38ae2a2a91
Rallph <rallph@protonmail.com>
2018-10-07 10:44:23 -0500

    fixed positioning of map

--- parkhawk/parkmap/static/parkmap/js/map.js
+++ parkhawk/parkmap/static/parkmap/js/map.js
@@ -1,8 +1,8 @@
 function initMap() {
-    let myLatLng = {lat: -25.363, lng: 131.044};
+    let myLatLng = {lat: 41.661129, lng: -91.530167};
 
     let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 14,
       center: myLatLng
     });
 
