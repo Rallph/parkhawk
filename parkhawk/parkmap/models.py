e95ea0ac73b098ffde2efe1bc524fe05cf94af73
Kaleb McClenathan <kaleb.mcclenathan@gmail.com>
2018-10-06 20:00:26 -0500

    added latitude and longitude as fields, changed 'location' to 'address'

--- parkhawk/parkmap/models.py
+++ parkhawk/parkmap/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class ParkingSpot(models.Model):
-    location = models.CharField(max_length=140) # street, possibly address
+    address = models.CharField(max_length=140) # address
+    latitude = models.DecimalField(max_digits=3,decimal_places=7) # latitude of location
+    longitude = models.DecimalField(max_digits=3,decimal_places=7) # longitude of location
     price = models.DecimalField(max_digits=5,decimal_places=2) # price of space
     hours_start_time = models.TimeField() # no way to represent time range that I could find
     hours_end_time = models.TimeField()   # so making start hour and end hour separate
