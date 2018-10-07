8f67adcf5d62fd4d0dc90a7dccd3a17b47d89229
Kaleb McClenathan <kaleb.mcclenathan@gmail.com>
2018-10-06 21:15:10 -0500

    changed the parkingspot model

--- parkhawk/parkmap/models.py
+++ parkhawk/parkmap/models.py
@@ -11,4 +11,4 @@ class ParkingSpot(models.Model):
     note = models.CharField(max_length=500) # note for listing creator to leave for others
 
     def __str__(self):
-        return "{0}\t{1}\t{2}\t{3}\t{4}".format(self.location, self.price, self.hours_start_time, self.hours_end_time, self.note)
+        return "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}".format(self.address, self.latitude, self.longitude, self.price, self.hours_start_time, self.hours_end_time, self.note)
