89db39c68d8d2158682aa9be4a270a18daa43cd0
Rallph <rallph@protonmail.com>
2018-10-06 20:26:15 -0500

    make db form

--- parkhawk/parkmap/urls.py
+++ parkhawk/parkmap/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('form/', views.parking_spot_create_view)
     
 ]
--- parkhawk/parkmap/views.py
+++ parkhawk/parkmap/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import ParkingSpot
+from .forms import ParkingSpotForm
+
 
 # Create your views here.
 def index(request):
     return render(request, 'parkmap/base.html')
+
+def parking_spot_create_view(request):
+    form = ParkingSpotForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    
+    context = {'form': form}
+
+    return render(request, 'parkmap/form.html', context)
